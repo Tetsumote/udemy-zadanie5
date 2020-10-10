e9c3b5e30e944c78b50652fb03245850849a173e
Paweł Grajewski <unionproject88@gmail.com>
2020-10-10 22:55:27 +0200

    add redux to application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
-import "./styles.css";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import Persons from "./containers/Persons";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ol>
+          <li>
+            Turn this app into one which does NOT use local state (in
+            components) but instead uses Redux
+          </li>
+        </ol>
+        <Persons />
+      </div>
+    );
+  }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./App";
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   rootElement
 );
